f764f2140cbcc4b178af6c70425ecab397b8094c
jnardi86 <julian.nardi@gmail.com>
2024-02-20 10:02:08 -0300

    Add new Style to SpeakerCard

--- src/pages/home/components/homeSpeakers/components/speakerCard/SpeakerCard.jsx
+++ src/pages/home/components/homeSpeakers/components/speakerCard/SpeakerCard.jsx
@@ -10,17 +10,17 @@ const SpeakerCard = ({ name: name, speciality: speciality, review: review, image
   }
 
   return (
-    <div className="w-[90%] h-[550px] mx-auto mb-5 flex flex-col items-center justify-center border-solid border-4 border-White group-hover:border-Blue rounded-xl">
+    <div className="w-[90%] h-[550px] mx-auto mb-5 flex flex-col items-center justify-center bg-Blue border-solid border-4 border-White rounded-xl">
       <div
         className="w-full h-64 pt-1 flex items-center justify-center"
       >
         <img src={image} className="object-cover rounded-full w-52 h-52" alt="Speaker Image" />
       </div>
       <div className="w-full mb-5 p-5 flex flex-col justify-center items-center rounded-xl md:px-2 md:py-5">
-        <h3 className="text-xl p-2 font-montserrat font-Bold text-White group-hover:text-Blue md:text-4xl">{name}</h3>
-        <h5 className="text-base font-montserrat font-Semibold mb-3 text-White group-hover:text-Blue md:text-xl">{speciality}</h5>
+        <h3 className="text-xl p-2 font-montserrat font-Bold text-White md:text-4xl">{name}</h3>
+        <h5 className="text-base font-montserrat font-Semibold mb-3 text-White desktop: md:text-xl">{speciality}</h5>
         <p
-          className="font-montserratAlternate font-Regular text-base leading-relaxed md:text-xl text-White group-hover:text-Blue">
+          className="font-montserratAlternate font-Regular text-base leading-relaxed md:text-xl text-White">
             {/* {showFullReview ? review : `${review.slice(0, 150)}...`} */}
             {review}
         </p>
--- src/pages/home/components/homeSpeakers/view/SpeakerView.jsx
+++ src/pages/home/components/homeSpeakers/view/SpeakerView.jsx
@@ -3,9 +3,9 @@ import SpeakerCarousel from "../components/speakerCard/SpeakerCarousel"
 
 const SpeakerView = () => {
   return (
-    <div className="w-full mx-auto mb-40 flex flex-col bg-Blue group hover:bg-Green rounded-xl shadow-md md:h-[730px]">
+    <div className="w-full mx-auto mb-40 flex flex-col bg-Blue group desktop:hover:bg-Green rounded-xl shadow-md md:h-[730px]">
       <div className="w-full text-center p-5">
-        <h2 className='font-montserrat font-Bold text-5xl mb-10 text-White group-hover:text-Blue'>ORADORES</h2>
+        <h2 className='font-montserrat font-Bold text-5xl mb-10 text-White desktop:group-hover:text-Blue'>ORADORES</h2>
       </div>
       <SpeakerCarousel />
     </div>
