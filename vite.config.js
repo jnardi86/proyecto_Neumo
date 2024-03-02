7ee84b56a600a4aaa6112e9f6b64d13cf8cac9c5
titojp <titojp2002@hotmail.com>
2024-03-02 12:36:25 -0300

    build para sitio wen

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://jnardi86.github.io/proyecto_Neumo'
+  // base: 'https://jnardi86.github.io/proyecto_Neumo'
+  // agregar esta linea a package-json "homepage": "https://github.com/jnardi86/proyecto_Neumo",
 })
